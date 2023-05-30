a5e4f6c2e7e6b3856547965d3e9fd138911fe322
Travis Vachon <travis.vachon@gmail.com>
2023-05-30 14:05:29 -0700

    add "go to customer" form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client'
-
+import type { EmailAddress } from "@web3-storage/did-mailto/dist/src/types";
 import { useState, useCallback, ChangeEvent, FormEvent } from "react"
 import { useRouter } from "next/navigation";
+import * as MailtoDid from '@web3-storage/did-mailto'
 
 export default function Root () {
   const router = useRouter();
@@ -9,15 +10,25 @@ export default function Root () {
   const goToSpace = useCallback((e: FormEvent) => {
     e.preventDefault()
     if (spaceDID) {
-      console.log("going to ", `/space/${spaceDID.toString()}`)
       router.push(`/spaces/${spaceDID.toString()}`)
     }
   }, [spaceDID])
+  const [customerEmail, setCustomerEmail] = useState<EmailAddress | undefined>()
+  const goToCustomer = useCallback((e: FormEvent) => {
+    e.preventDefault()
+    if (customerEmail) {
+      router.push(`/customers/${MailtoDid.fromEmail(customerEmail)}`)
+    }
+  }, [customerEmail])
   return (
       <div className='flex flex-col items-center'>
         <h1 className='text-xl mb-10'>Admin</h1>
-        <form onSubmit={goToSpace} className='flex flex-col space-y-2'>
+        <form onSubmit={goToSpace} className='flex flex-col space-y-2 mb-16'>
           <input className='text-black py-1 px-2 rounded' type='text' placeholder="Space DID" onChange={(e: ChangeEvent<HTMLInputElement>) => setSpaceDID(e.target.value)} />
+          <input className='w3ui-button' type='submit' value='Go'/>
+        </form>
+        <form onSubmit={goToCustomer} className='flex flex-col space-y-2'>
+          <input className='text-black py-1 px-2 rounded' type='text' placeholder="Customer Email" onChange={(e: ChangeEvent<HTMLInputElement>) => setCustomerEmail(e.target.value)} />
           <input className='w3ui-button' type='submit' value='Go'/>
         </form>
       </div>
